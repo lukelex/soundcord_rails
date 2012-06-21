a75b2bf2d30af5851bf07c33c9c03ad5dd1ad4e3
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-21 02:45:09 -0300

    Adding initializer generator

---
+++ lib/generators/initializer_generator.rb
@@ -0,0 +1,8 @@
+class InitializerGenerator < Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+
+  desc "This generator creates an initializer file at config/initializers"
+  def copy_initializer_file
+    copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
+  end
+end
