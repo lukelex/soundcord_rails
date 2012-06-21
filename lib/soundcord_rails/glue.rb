4f4f9ec325a22f49a6d5c42814e0dd296b28821b
Lukas Alexandre <lukeskytm@gmail.com>
2012-06-21 02:26:23 -0300

    Removing unused code

--- lib/soundcord_rails/glue.rb
+++ lib/soundcord_rails/glue.rb
@@ -3,9 +3,6 @@ module SoundcordRails
     def self.included(base)
       base.extend ClassMethods
       base.class_attribute :phonetized_definitions
-
-      # locale_path = Dir.glob(File.dirname(__FILE__) + "/locales/*.{rb,yml}")
-      # I18n.load_path += locale_path unless I18n.load_path.include?(locale_path)
     end
   end
 end
--- soundcord_rails.gemspec
+++ soundcord_rails.gemspec
@@ -18,6 +18,5 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  # s.add_development_dependency "soundcord"
   s.add_dependency 'soundcord', '~> 0.1.1'
 end
